f02c80c0cca2ce0d6ebaad267f5eefa4874f59b8
xaviergodbout <48611188+xaviergodbout@users.noreply.github.com>
2025-06-11 19:52:32 -0400

    FUCK CHAT GPT - REMOVED DYNAMIC NAV

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,6 @@
 const markdownIt = require("markdown-it");
 
 module.exports = function (eleventyConfig) {
-    // Add pages to NAV
-    eleventyConfig.addCollection("pages", function(collectionApi) {
-        return collectionApi.getAll().filter(item => {
-          return item.inputPath.startsWith("./src/") &&
-                 item.inputPath.endsWith(".njk") &&
-                 item.data.permalink; // Only get pages with permalink
-        });
-      });
-
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/js");
     eleventyConfig.addPassthroughCopy("src/img");
