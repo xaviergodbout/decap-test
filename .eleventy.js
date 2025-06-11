1423375e6175b698baca134d4f1b7798bd1f661f
xaviergodbout <48611188+xaviergodbout@users.noreply.github.com>
2025-06-11 17:19:25 -0400

    Added markup to html

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,28 @@
+const markdownIt = require("markdown-it");
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/js");
     eleventyConfig.addPassthroughCopy("src/img");
     eleventyConfig.addPassthroughCopy("src/admin");
+
+    // Initialize Markdown-It
+    const markdownLib = markdownIt({ html: true });
+
+    // Add a custom filter named "markdown"
+    eleventyConfig.addFilter("markdown", (value) => {
+        return markdownLib.render(value || "");
+    });
+
+    // Optional but recommended: use this library globally
+    eleventyConfig.setLibrary("md", markdownLib);
+
     return {
         dir: {
             input: "src", // Source files
             output: "public", // Output files
+            includes: "_includes",
+            data: "_data"
         },
     };
 };
