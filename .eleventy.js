75679703cd013e96804d78d38839481e67783d72
xaviergodbout <48611188+xaviergodbout@users.noreply.github.com>
2025-06-11 14:57:25 -0400

    Fixed CMS

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/js");
     eleventyConfig.addPassthroughCopy("src/img");
+    eleventyConfig.addPassthroughCopy("src/admin");
     return {
         dir: {
             input: "src", // Source files
