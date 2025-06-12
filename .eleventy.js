d454e7a20d1fc39527fd1522682497e2ad9378b3
x-g <xa.godbout@gmail.com>
2025-06-12 18:34:05 -0400

    Fixed rollout

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,10 @@
 const markdownIt = require("markdown-it");
-const { customMarkdown } = require('./src/_data/customFilters');
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/js");
     eleventyConfig.addPassthroughCopy("src/img");
     eleventyConfig.addPassthroughCopy("src/admin");
-    eleventyConfig.addPassthroughCopy("public");
 
     // Initialize Markdown-It
     const markdownLib = markdownIt({ html: true });
@@ -15,9 +13,6 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addFilter("markdown", (value) => {
         return markdownLib.render(value || "");
     });
-
-    // Add custom markdown filter
-    eleventyConfig.addFilter("customMarkdown", customMarkdown);
 
     // Optional but recommended: use this library globally
     eleventyConfig.setLibrary("md", markdownLib);
