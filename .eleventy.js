03b436a366aecb598b66720ffec822281a3cd7b4
xaviergodbout <48611188+xaviergodbout@users.noreply.github.com>
2025-06-15 00:14:17 -0400

    Added FK Rasrter Roman

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/js");
     eleventyConfig.addPassthroughCopy("src/img");
     eleventyConfig.addPassthroughCopy("src/admin");
+    eleventyConfig.addPassthroughCopy("src/fonts");
 
     // Initialize Markdown-It
     const markdownLib = markdownIt({ html: true });
