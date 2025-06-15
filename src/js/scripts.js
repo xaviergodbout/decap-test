b18d67b6016639cf056efb55a7890484aef27a18
xaviergodbout <48611188+xaviergodbout@users.noreply.github.com>
2025-06-15 16:26:02 -0400

    removed js

--- src/js/scripts.js
+++
@@ -1,3 +0,0 @@
-// Update theme color dynamically
-const bgColor = getComputedStyle(document.body).backgroundColor;
-document.querySelector('meta[name="theme-color"]').setAttribute('content', bgColor);
